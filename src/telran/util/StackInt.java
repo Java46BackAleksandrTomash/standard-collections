97d8b9e8e8c83e8a1177037e1140a755423e3ddb
aleksandrtomash <tombasket508@gmail.com>
2022-09-20 15:00:22 +0300

    HW-26 right solution of Stack .methods pop,push,getMaxNumber

--- src/telran/util/StackInt.java
+++ src/telran/util/StackInt.java
@@ -3,39 +3,74 @@ package telran.util;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-
+import java.util.LinkedList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+//public class StackInt {
+//
+//	List<Integer> list = new ArrayList<>();
+//
+//	public int pop() {
+//		if (list.isEmpty()) {
+//			throw new NoSuchElementException();
+//		}
+//		return list.remove((list.size() - 1));
+//	}
+//
+//	public void push(int number) {
+//
+//		if (list.isEmpty() || number >= list.size() - 1) {
+//			list.add(number);
+//		}
+//	}
+//
+//	public boolean isEmpty() {
+//
+//		return list.isEmpty();
+//	}
+//
+//	public int getMaxNumber() {
+//		if (list.isEmpty()) {
+//			throw new NoSuchElementException();
+//		}
+//		Collections.sort(list);
+//
+//		return (list.get(list.size() - 1));
+//	}
+//}
 public class StackInt {
-
-	List<Integer> list = new ArrayList<>();
-
-	public int pop() {
-		if (list.isEmpty()) {
-			throw new NoSuchElementException();
-		}
-		return list.remove((list.size() - 1));
+	private LinkedList<Integer> numbers = new LinkedList<>();
+	private LinkedList<Integer> maxNumbers = new LinkedList<>();
+public int pop() {
+	//removes last number and returns removed number
+	//throws exception NoSuchElementException for empty stack
+	if (numbers.isEmpty()) {
+		throw new NoSuchElementException();
 	}
-
-	public void push(int number) {
-
-		if (list.isEmpty() || number >= list.size() - 1) {
-			list.add(number);
-		}
+	int res = numbers.removeLast();
+	if (res == maxNumbers.getLast()) {
+		maxNumbers.removeLast();
 	}
-
-	public boolean isEmpty() {
-
-		return list.isEmpty();
+	return res;
+}
+public void push(int number) {
+	//adds number at end of a stack
+	numbers.add(number);
+	if (maxNumbers.isEmpty() || number >= maxNumbers.getLast()) {
+		maxNumbers.add(number);
 	}
-
-	public int getMaxNumber() {
-		if (list.isEmpty()) {
-			throw new NoSuchElementException();
-		}
-		Collections.sort(list);
-
-		return (list.get(list.size() - 1));
+}
+public boolean isEmpty() {
+	//returns true if a stack is empty
+	return numbers.isEmpty();
+}
+public int getMaxNumber() {
+	//returns maximal number existing in a stack
+	//throws exception NoSuchElementException for empty stack
+	if (maxNumbers.isEmpty()) {
+		throw new NoSuchElementException();
 	}
+	return maxNumbers.getLast();
+}
 }
