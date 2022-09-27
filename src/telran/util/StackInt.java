ab19155ae22994ce711e621fafffe39ec74cad0b
aleksandrtomash <tombasket508@gmail.com>
2022-09-27 09:57:28 +0300

    hw #28 HashSet, TreeSet Interview questions

--- src/telran/util/StackInt.java
+++ src/telran/util/StackInt.java
@@ -1,44 +1,8 @@
 package telran.util;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.NoSuchElementException;
 
-//public class StackInt {
-//
-//	List<Integer> list = new ArrayList<>();
-//
-//	public int pop() {
-//		if (list.isEmpty()) {
-//			throw new NoSuchElementException();
-//		}
-//		return list.remove((list.size() - 1));
-//	}
-//
-//	public void push(int number) {
-//
-//		if (list.isEmpty() || number >= list.size() - 1) {
-//			list.add(number);
-//		}
-//	}
-//
-//	public boolean isEmpty() {
-//
-//		return list.isEmpty();
-//	}
-//
-//	public int getMaxNumber() {
-//		if (list.isEmpty()) {
-//			throw new NoSuchElementException();
-//		}
-//		Collections.sort(list);
-//
-//		return (list.get(list.size() - 1));
-//	}
-//}
 public class StackInt {
 	private LinkedList<Integer> numbers = new LinkedList<>();
 	private LinkedList<Integer> maxNumbers = new LinkedList<>();
