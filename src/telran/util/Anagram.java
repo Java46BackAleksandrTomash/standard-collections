def2e3fa01f93d5756d8470a4ca419064ebe72e7
aleksandrtomash <tombasket508@gmail.com>
2022-09-29 18:56:00 +0300

    HW 29 final solution MyArray and Anagram

--- src/telran/util/Anagram.java
+++ src/telran/util/Anagram.java
@@ -28,5 +28,34 @@ public class Anagram {
 
 		return true;
 	}
-
+	///////////////////// another solution///////////////////
+	
+//	public static boolean isAnagram1(String word, String anagram) {
+//		boolean res = false;
+//		if(word.length() == anagram.length()) {
+//			HashMap<Character, Integer> mapLetters = getMapLetters(word);
+//			res = true;
+//			for(char letter: anagram.toCharArray()) {
+//				Integer count = mapLetters.getOrDefault(letter, 0);
+//				if(count == 0) {
+//					res = false;
+//					break;
+//				}
+//				mapLetters.put(letter, count - 1);
+//			}
+//			
+//		}
+//		
+//	return res;
+//}
+//
+//private static HashMap<Character, Integer> getMapLetters(String word) {
+//	HashMap<Character, Integer> res = new HashMap<>();
+//	for(char letter: word.toCharArray()) {
+//		Integer count = res.getOrDefault(letter, 0);
+//		res.put(letter, count + 1);
+//	}
+//	return res;
+//}
 }
+
--- src/telran/util/MyArray.java
+++ src/telran/util/MyArray.java
@@ -3,40 +3,82 @@ package telran.util;
 import java.util.HashMap;
 import java.util.Map;
 
+//public class MyArray<T> {
+//
+//	public int size;
+//	private T valueToSet;
+//	private Map<Integer, T> mapValues = new HashMap<>();
+//
+//	public MyArray(int size) {
+//		if (size < 0) {
+//			throw new IllegalArgumentException();
+//		}
+//		this.size = size;
+//	}
+//
+//	public void setAll(T value) {
+//		//mapValues = new HashMap<>();
+//		valueToSet = value;
+//	}
+//
+//	public T get(int index) {
+//		if (!isIndexTrue(index)) {
+//			throw new ArrayIndexOutOfBoundsException();
+//
+//		}
+//		return mapValues.get(index);
+//	}
+//
+//	public void set(int index, T value) {
+//		if (!isIndexTrue(index)) {
+//			throw new ArrayIndexOutOfBoundsException();
+//	}
+//		mapValues.put(index, value);
+//	}
+//	
+//	public boolean isIndexTrue(int index) {
+//		return index >= 0 && index < size;
+//	}
+//}
 public class MyArray<T> {
-
+	private T allValues;
 	private int size;
-	private T valueToSet;
-	private Map<Integer, T> mapValues = new HashMap<>();
-
+	private HashMap<Integer, T> mapOfSets;
 	public MyArray(int size) {
-		if (size < 0) {
-			throw new IllegalArgumentException();
-		}
 		this.size = size;
+		mapOfSets = new HashMap<>();
 	}
-
-	public void setAllValue(T value) {
-
-		valueToSet = value;
+	/**
+	 * sets all array's elements with a given value
+	 * @param value
+	 */
+	public void setAll(T value) {
+		mapOfSets = new HashMap<>();
+		allValues = value;
+		
 	}
-
+	/**
+	 * 
+	 * @param index
+	 * @return value at given index or null if index is wrong
+	 */
 	public T get(int index) {
-		if (!isIndexTrue(index)) {
-			throw new ArrayIndexOutOfBoundsException();
-
+		T res = null;
+		if (index > -1 && index < size) {
+			res = mapOfSets.getOrDefault(index, allValues);
 		}
-		return mapValues.get(index);
+		return res;
 	}
-
+	/**
+	 * sets a given value at a given index
+	 * throws IndexOutOfBoundsException in the case of wrong index
+	 * @param index
+	 * @param value
+	 */
 	public void set(int index, T value) {
-		if (!isIndexTrue(index)) {
-			throw new ArrayIndexOutOfBoundsException();
-	}
-		mapValues.put(index, value);
-	}
-	
-	public boolean isIndexTrue(int index) {
-		return index >= 0 && index < size;
+		if (index < 0 || index >= size) {
+			throw new IndexOutOfBoundsException(index);
+		}
+		mapOfSets.put(index, value);
 	}
 }
